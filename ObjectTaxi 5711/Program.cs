3ea3363403d671e4e3ae4efedd566d7991af6e70
ZaakyReal12 <zaakyreal@gmail.com>
2024-03-20 13:56:33 +0700

    first commit

---
+++ ObjectTaxi 5711/Program.cs
@@ -0,0 +1,31 @@
+﻿using ObjectTaxi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectTaxi_5711
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // membuat objek Taxi 
+            Taxi taxi = new Taxi();
+
+            // mengeset nilai properties objek Taxi 
+            taxi.DriverName = "Zaaky";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 12;
+
+            // memanggil method 
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ObjectTaxi 5711/Taxi.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectTaxi
+{
+    internal class Taxi
+    {
+        // propperties 
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public float NumPassenger { get; set; }
+
+        // method 
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Nama         : {0}", DriverName);
+            Console.WriteLine("On Duty             : {0}", OnDuty);
+            Console.WriteLine("Number of Passenger : {0}", NumPassenger);
+            Console.WriteLine();
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} sedang menjemput penumpang", DriverName);
+        }
+
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} selesai mengantar penumpang", DriverName);
+        }
+    }
+}
